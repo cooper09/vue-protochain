80fea8fd7b4f460f92b728ef0cdfec65541f586c
cooper09 <cooper@sonyainc.net>
2021-05-01 13:59:26 -0400

    add content view to blockchain

--- src/views/Consumer.vue
+++ src/views/Consumer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="animated fadeIn">
-    <v-btn class="closeBtn" @click="closeMe">X</v-btn>
+          <span class="right"><v-btn                   outlined
+                  raised
+                  primary
+                  color="indigo"  @click="closeMe()">Exit</v-btn></span>
     <h1>Consumer Page</h1>
     <br/><br/>
     <v-tabs>
@@ -18,7 +21,7 @@
     </p>
     <div class="videos">
       <ul class="videos__list">
-        <li v-for="(video, index) in videos" :key="index" class="videos__item" @click="playGoodie">
+        <li v-for="(video, index) in videos" :key="index" class="videos__item" @click="playGoodie()">
           <LazyYoutubeVideo
             :src="video.url"
             :preview-image-size="video.previewImageSize"
@@ -36,6 +39,8 @@
   </div>
 </template>
 <script>
+import {Transaction} from '../blockchain_token.js';
+import Pending from './Pending';
 import LazyYoutubeVideo from "vue-lazy-youtube-video";
 export default {
   components: {
@@ -50,17 +55,51 @@ export default {
           title: "Privi Protocol",
           address: "0x00",
           hash: "0x00"
-        },
-   
-      ]
-    };
+        }
+      ],
+      tx:{},
+      coin: {},
+      keys: {},
+      from:'',
+      to: '',
+      amount: ''
+    };//end return
   },//end data
   methods: {
     showContent(){
       console.log("Show content...")
     },
     playGoodie() {
-      alert("Play goodie: " );
+      console.log("Consumer.playGoodie: ", this.coin);
+      console.log("Consumer.playGoodies - keys: ", this.keys)
+      /* 
+        1) create transaction between the current wallet the provider wallet. 
+        2) send transactoin to Pending for normal "procedure"...
+      */
+          //retrieve our public (walletPublicAddr) and  () key
+      const walletKey = this.keys.walletKey;
+      const walletPublicAddr = this.keys.walletPublicAddr;
+
+    const timeDuration = 288  //in seconds
+    let approxCost = timeDuration / 10;
+      const newTx = new Transaction(walletPublicAddr,this.keys.publicKey, approxCost);
+      console.log("our New transaction: ", newTx );
+
+    //sign transaction with the private key
+      newTx.signTransaction(walletKey); 
+      console.log("Consumer - Signed transaction: ", newTx.signature );
+
+      console.log("Time to go to work: ", newTx )
+        // see if I can send an event up the chain....
+        //this,$emit('begin-minin', this.tx )
+          
+      this.coin.addTransaction(newTx);
+
+          //mine up the new transaction in its own block
+    console.log("\n Starting up the 7 dwarfs...");
+
+    this.coin.minePendingTransactions(this.keys);
+
     },
     foo() {
       console.log("Video has loaded...");
@@ -68,7 +107,15 @@ export default {
     closeMe(){
       this.$router.push('/');
     }
-  }//end methods
+  },//end methods
+    created () {
+      console.log("Consumer - Begin CreateTransaction!");
+      this.coin = this.$store.getters.getCoin;
+      console.log("Consumer - Coin in the store: ", this.coin.miningReward );
+
+      this.keys = this.$store.getters.getKeys;
+  
+  }//end playGoodie
 }//export
 </script>
 <style scoped>
